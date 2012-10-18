7683fdae2892864dc8bd43575755592b1d2270f8
andriy-gerasika <andriy.gerasika@gmail.com>
2012-10-18 20:13:35 +0300

    actualize

--- src/com/gerixsoft/objc2xml/ObjectiveCToXml.java
+++ src/com/gerixsoft/objc2xml/ObjectiveCToXml.java
@@ -1,9 +1,8 @@
 package com.gerixsoft.objc2xml;
 
 import java.io.File;
-import java.util.Arrays;
-
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.sax.SAXSource;
@@ -11,36 +10,38 @@ import javax.xml.transform.stream.StreamResult;
 
 import org.xml.sax.InputSource;
 
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
-
 public class ObjectiveCToXml {
 
 	public static void main(String[] args) throws TransformerException {
 		if (args.length != 2) {
-			System.err.println("usage: <objc-source> <xml-result>");
+			System.err.println("usage: <input-objc-file-or-directory> <output-xml-file-or-directory>");
 			System.exit(-1);
 		}
-		File objcFile = new File(args[0]);
-		File xmlFile = new File(args[1]);
-
-		if (objcFile.isFile()) {
-			objc2xml(objcFile, xmlFile);
-		} else {
-			File[] files = objcFile.listFiles();
-			Arrays.sort(files);
-			for (File file : files) {
-				System.out.println(file);
-				objc2xml(file, new File(xmlFile, file.getName() + ".xml"));
-			}
-		}
+		ObjectiveCToXml objc2xml = new ObjectiveCToXml();
+		objc2xml.transformer.setOutputProperty("indent", "yes");
+		objc2xml.transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+		objc2xml.transform(new File(args[0]), new File(args[1]));
 		System.out.println("ok");
 	}
 
-	public static void objc2xml(File objcFile, File xmlFile) throws TransformerException {
-		Transformer transformer = TransformerFactory.newInstance().newTransformer();
-		transformer.setOutputProperty("indent", "yes");
-		transformer.transform(
-				new SAXSource(new ObjectiveCSAXParser(), new InputSource(objcFile.toString())),
-				new StreamResult(xmlFile));
+	private Transformer transformer;
+
+	public ObjectiveCToXml() throws TransformerConfigurationException {
+		transformer = TransformerFactory.newInstance().newTransformer();
+	}
+
+	public void transform(File input, File output) throws TransformerException {
+		if (input.isDirectory()) {
+			output.mkdir();
+			for (File child : input.listFiles()) {
+				if (child.isDirectory()) {
+					transform(child, new File(output, child.getName()));
+				} else if (child.getName().endsWith(".h")) {
+					transform(child, new File(output, child.getName().concat(".xml")));
+				}
+			}
+			return;
+		}
+		transformer.transform(new SAXSource(new ObjectiveCSaxParser(), new InputSource(input.toString())), new StreamResult(output));
 	}
 }
